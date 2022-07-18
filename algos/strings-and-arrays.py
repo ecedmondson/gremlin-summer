c4e9692bf649124cb9b57f01d345b34154cee2ef
Rachael Wisecarver <rachael@bevy.com>
2022-07-18 11:06:34 -0700

    adding validation

--- algos/strings-and-arrays.py
+++ algos/strings-and-arrays.py
@@ -6,6 +6,10 @@ def is_substring(string1, string2):
 
 def is_rotation(string1, string2):
     """Determine if string2 is a rotation of string1"""
+    if not string1 or not string2:
+        return False
+    if len(string2) != len(string1):
+        return False
     double_trouble = string2 + string2
     return is_substring(string1, double_trouble)
 
