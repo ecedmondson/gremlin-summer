8e6ec9be944c5f56d611cf4ff0f4f131f969de7b
Rachael Wisecarver <rachael@bevy.com>
2022-07-18 10:44:24 -0700

    adding new fns, directory structure

--- algos/strings-and-arrays.py
+++ algos/strings-and-arrays.py
@@ -1,1 +1,11 @@
 """String and array algorithm practice"""
+
+def is_substring(string1, string2):
+    """Determine if string1 is a substring of string2"""
+    return string1 in string2
+
+def is_rotation(string1, string2):
+    """Determine if string2 is a rotation of string1"""
+    double_trouble = string2 + string2
+    return is_substring(string1, double_trouble)
+
